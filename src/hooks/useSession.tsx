b46ecb5a31baaf94ae712b7065f55a69f64e3a80
vacom <va.works@hotmail.com>
2021-02-14 15:06:00 +0000

    fix: fix typo on useSession hook

--- src/hooks/useSession.tsx
+++ src/hooks/useSession.tsx
@@ -4,7 +4,7 @@ import { AuthContext, AuthContextProps } from "context/AuthContext";
 export const useSession = (): AuthContextProps => {
   const context = useContext(AuthContext);
   if (context === undefined) {
-    throw new Error(`useUser must be used within a UserContextProvider.`);
+    throw new Error(`useSession must be used within a AuthContextProvider.`);
   }
   return context;
 };
