a0fe39bff60d5c9de523ea0381f6e397453532ec
amaralflavio <fara-16@hotmail.com>
2021-02-24 21:30:11 +0000

    chore: create notification component

---
+++ src/theme/constants.ts
@@ -0,0 +1,42 @@
+export const colors = {
+  primary: "#276EF1",
+  red600: "#870F00",
+  danger: "#E11900", // red400
+  red300: "#E85C4A",
+  red200: "#F1998E",
+  success: "#05944F",
+  blue400: "#276EF1",
+  blue200: "#A0BFF8",
+  blue300: "#5B91F5",
+  yellow400: "#FFC043",
+  yellow200: "#FFE3AC",
+  orange400: "#FF6937",
+  orange200: "#FABDA5",
+  purple400: "#7356BF",
+  purple200: "#C1B5E3",
+  green400: "#05944F",
+  green200: "#66D19E",
+  gray50: "#F6F6F6",
+  gray100: "#EEEEEE",
+  gray200: "#E2E2E2",
+  gray300: "#CBCBCB",
+  gray400: "#AFAFAF",
+  gray500: "#757575",
+  gray600: "#545454",
+  gray700: "#333333",
+  gray800: "#1F1F1F",
+  gray900: "#141414",
+  white: "#FFFFFF",
+  primaryUnderlayColor: "#A0BFF8",
+  // most cases feedback colors increment or decrement by 200
+  mainUnderlayColor: "#E2E2E2",
+  shadowColor: "#141414",
+};
+
+export const sizes = {
+  borderRadius: {
+    small: "5px",
+    default: "10px",
+    large: "30px",
+  },
+};
