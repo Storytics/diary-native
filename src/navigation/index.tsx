9a566a68c2660600c09651a00c988ce31ec02861
amaralflavio <fara-16@hotmail.com>
2021-03-21 17:14:53 +0000

    fix: #58 potential fix for flickering

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -16,6 +16,7 @@ import PrivacyScreen from "screens/Legal/Privacy";
 import TermsScreen from "screens/Legal/Terms";
 // Types
 import { RootStackParamList } from "types/navigation";
+import { useTheme } from "styled-components";
 
 const Stack = createStackNavigator<RootStackParamList>();
 
@@ -25,19 +26,30 @@ export const navigate = (name: string, params?: RootStackParamList) => {
   navigationRef.current?.navigate(name, params);
 };
 
-const Navigation: React.FC = () => (
-  <NavigationContainer ref={navigationRef}>
-    <Stack.Navigator initialRouteName="Home" headerMode="none" mode="card">
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Diary" component={DiaryScreen} />
-      <Stack.Screen name="Editor" component={EditorScreen} />
-      <Stack.Screen name="Cloud" component={CloudScreen} />
-      <Stack.Screen name="Password" component={PasswordScreen} />
-      <Stack.Screen name="Billing" component={BillingScreen} />
-      <Stack.Screen name="Terms" component={TermsScreen} />
-      <Stack.Screen name="Privacy" component={PrivacyScreen} />
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+const Navigation: React.FC = () => {
+  const theme = useTheme();
+  return (
+    <NavigationContainer ref={navigationRef}>
+      <Stack.Navigator
+        initialRouteName="Home"
+        headerMode="none"
+        mode="card"
+        screenOptions={{
+          headerStyle: { elevation: 0 },
+          cardStyle: { backgroundColor: theme.container.backgroundColor },
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Diary" component={DiaryScreen} />
+        <Stack.Screen name="Editor" component={EditorScreen} />
+        <Stack.Screen name="Cloud" component={CloudScreen} />
+        <Stack.Screen name="Password" component={PasswordScreen} />
+        <Stack.Screen name="Billing" component={BillingScreen} />
+        <Stack.Screen name="Terms" component={TermsScreen} />
+        <Stack.Screen name="Privacy" component={PrivacyScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default Navigation;
